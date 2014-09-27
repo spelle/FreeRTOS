b793eb44b051c6d79d0861def8ce300f5bc0a58e
Sébastien PELLÉ <pelle.sebastien@gmail.com>
2014-09-27 22:41:02 +0200

    completing FreeRTOS ATmega328P port

--- FreeRTOS/Source/portable/GCC/ATMega328/port.d
+++
@@ -1,6 +0,0 @@
-port.o port.d : FreeRTOS/Source/portable/GCC/ATMega328/port.c \
- FreeRTOS/Source/include/FreeRTOS.h FreeRTOSConfig.h \
- FreeRTOS/Source/include/projdefs.h FreeRTOS/Source/include/portable.h \
- FreeRTOS/Source/include/../portable/GCC/ATMega328/portmacro.h \
- FreeRTOS/Source/include/mpu_wrappers.h FreeRTOS/Source/include/task.h \
- FreeRTOS/Source/include/list.h
